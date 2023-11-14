7beea7ef98b1918ae6d4a2a3f7a12b3d2a0156bb
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2023-11-14 09:08:44 +0100

    test on another page

--- CornersBetting.py
+++ CornersBetting.py
@@ -89,11 +89,10 @@ team = st.selectbox("Choose the team", pd.Series(team_codes['team_name']))
 if team != "":
     code = team_codes.team_code[team_codes.team_name == team].reset_index(drop=True)[0]
 
-mws1 = import_MyWebScrapingTools().MyWsTools(chromedriver_executable_path=get_chromedriver_path(), driver_headless=True, driver_loglevel3=True, driver_noImg=True)
-driver1 = mws.driver
-
 def single_team(code, team):
-    driver1.get(f"https://fbref.com/en/squads/{code}/2023-2024/matchlogs/c11/passing_types/{team}-Match-Logs-Serie-A")
+    # driver.get(f"https://fbref.com/en/squads/{code}/2023-2024/matchlogs/c11/passing_types/{team}-Match-Logs-Serie-A")
+    driver.get(f"https://docs.streamlit.io/knowledge-base/tutorials/deploy/docker")
+    st.write(mws.element('/html/body/div/main/div/section/section[2]/article/div[2]/h2[1]/a', text = True))
     team_corners_table = pd.merge(corners_for(), corners_against(), left_index=True, right_index=True, suffixes=('', '_y'))
     team_corners_table = team_corners_table.loc[:, ~team_corners_table.columns.isin(["Date_y","Round_y","Venue_y","Result_y","GF_y","GA_y","Opponent_y"])]
     team_corners_table["Outcome"] = team_corners_table.apply(lambda row: 'Win' if row['Corners for'] > row['Corners against'] else ('Draw' if row['Corners for'] == row['Corners against'] else 'Defeat'), axis=1) # create 1X2 column
