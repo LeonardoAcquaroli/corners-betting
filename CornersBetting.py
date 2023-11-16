a4882530da0685e7ffad329c65608fd5b4c5597e
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2023-11-16 15:44:33 +0100

    Update CornersBetting.py

--- CornersBetting.py
+++ CornersBetting.py
@@ -90,16 +90,13 @@ def t_test_predictions(teamA, teamB, alpha = 90.81):
         else:
             return ('X', p_value)
 
-driver.get('https://fbref.com/en/comps/11/schedule/Serie-A-Scores-and-Fixtures')
-
 while True:
     try:
+        driver.get('https://fbref.com/en/comps/11/schedule/Serie-A-Scores-and-Fixtures')
         fixtures_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="sched_2023-2024_11_1"]')))
         break
     except Exception as error:
         st.write(error) 
-
-
 
 fixtures =  pd.read_html((fixtures_table.get_attribute('outerHTML')))[0]
 fixtures = fixtures[fixtures.Wk.isna() == False] # delete the grey blank rows to separate gameweeks
